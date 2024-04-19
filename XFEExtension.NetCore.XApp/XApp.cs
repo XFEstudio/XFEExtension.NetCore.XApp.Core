101c0df60d2ab796838adeeddfc6a5cd2a750430
xfe18827675401 <xfe18827675401@126.com>
2024-04-19 11:55:33 +0800

    添加应用信息

--- XFEExtension.NetCore.XApp/XApp.cs
+++ XFEExtension.NetCore.XApp/XApp.cs
@@ -1,6 +1,17 @@
-﻿namespace XFEExtension.NetCore.XApp;
+﻿using XFEExtension.NetCore.FormatExtension;
+
+namespace XFEExtension.NetCore.XApp;
 
 public class XApp
 {
+    public XAPPInformation AppInfomation { get; set; }
     public List<XAppFile> AppFiles { get; set; }
+    public string Package()
+    {
+        var appDictionary = new XFEDictionary();
+        foreach (var appFile in AppFiles)
+        {
+            var fileDictionary = new XFEDictionary();
+        }
+    }
 }
---
+++ XFEExtension.NetCore.XApp/XAppInfo.cs
@@ -0,0 +1,54 @@
+﻿namespace XFEExtension.NetCore.XApp;
+
+/// <summary>
+/// XApp程序信息
+/// </summary>
+/// <param name="appIcon">app图标的Base64字符串</param>
+/// <param name="appName">应用名称</param>
+/// <param name="appVersion">应用版本</param>
+/// <param name="appDescription">描述</param>
+/// <param name="author">作者</param>
+/// <param name="company">公司</param>
+/// <param name="authorMail">作者电子邮件</param>
+/// <param name="projectWebSite">项目网站</param>
+/// <param name="projectGitHub">项目源码</param>
+public class XAPPInformation(string appIcon, string appName, Version appVersion, string? appDescription, string author, string? company, string? authorMail, string? projectWebSite, string? projectGitHub)
+{
+    /// <summary>
+    /// 应用图标的Base64字符串
+    /// </summary>
+    public string? AppIcon { get; init; } = appIcon;
+    /// <summary>
+    /// 应用名称
+    /// </summary>
+    public string AppName { get; init; } = appName;
+    /// <summary>
+    /// 应用版本
+    /// </summary>
+    public Version AppVersion { get; init; } = appVersion;
+    /// <summary>
+    /// 应用描述
+    /// </summary>
+    public string? AppDescription { get; init; } = appDescription;
+    /// <summary>
+    /// 作者
+    /// </summary>
+    public string? Author { get; init; } = author;
+    /// <summary>
+    /// 公司
+    /// </summary>
+    public string? Company { get; init; } = company;
+    /// <summary>
+    /// 作者电子邮件
+    /// </summary>
+    public string? AuthorMail { get; init; } = authorMail;
+    /// <summary>
+    /// 项目网站
+    /// </summary>
+    public string? ProjectWebSite { get; init; } = projectWebSite;
+    /// <summary>
+    /// 项目源码
+    /// </summary>
+    public string? ProjectGitHub { get; init; } = projectGitHub;
+}
+
